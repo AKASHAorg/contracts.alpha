691e8c71b0c5e773fda9cab627b900e257441988
Marius Darila <marius.darila@gmail.com>
2017-07-10 19:59:52 +0300

    refactor(token:model): add token states

--- contracts/token/AETH.sol
+++ contracts/token/AETH.sol
@@ -6,4 +6,17 @@ contract AETH is MintableToken, PausableToken {
     string public name = "AKASHA Token";
     string public symbol = "AETH";
     uint8 public decimals = 18;
+    enum AethState { Free, Bonded, Cycling }
+
+    mapping(address => mapping(uint8 => uint256)) tokenRecords;
+
+    function getTokenRecords(address _holder)
+    public
+    constant
+    returns(uint256 _free, uint256 _bonded, uint256 _cycling)
+    {
+        _free = tokenRecords[_holder][uint8(AethState.Free)];
+        _bonded = tokenRecords[_holder][uint8(AethState.Bonded)];
+        _cycling = tokenRecords[_holder][uint8(AethState.Cycling)];
+    }
 }
