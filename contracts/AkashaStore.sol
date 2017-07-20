6699dec25ecc689474f7d18e5b0c29dff2b05b9a
Marius Darila <marius.darila@gmail.com>
2017-07-20 19:23:08 +0300

    refactor(store): add generic get/set and store struct

--- contracts/AkashaStore.sol
+++ contracts/AkashaStore.sol
@@ -3,8 +3,51 @@ import './Bundled.sol';
 
 contract AkashaStore is Bundled {
 
+    address public previousStore;
+
+    struct Store {
+        mapping(uint => bytes32) uintBytes;
+        mapping(bytes32 => uint) bytesUint;
+    }
+
+    mapping(bytes32 => Store) state;
+
     function AkashaStore(address _owner) Bundled(_owner) {
 
+    }
+
+    function set(bytes32 _nameSpace, bytes32 _key, uint _value)
+    external
+    onlyModule
+    returns (bool)
+    {
+        state[_nameSpace].bytesUint[_key] = _value;
+
+        return state[_nameSpace].bytesUint[_key] == _value;
+    }
+
+    function set(bytes32 _nameSpace, uint _key, bytes32 _value)
+    external
+    onlyModule
+    returns (bool)
+    {
+        state[_nameSpace].uintBytes[_key] = _value;
+
+        return state[_nameSpace].uintBytes[_key] == _value;
+    }
+
+    function get(bytes32 _nameSpace, uint _key)
+    constant
+    returns (bytes32)
+    {
+        return state[_nameSpace].uintBytes[_key];
+    }
+
+    function get(bytes32 _nameSpace, bytes32 _key)
+    constant
+    returns (uint)
+    {
+        return state[_nameSpace].bytesUint[_key];
     }
 
 }
