64dd9919b28822ba9a6d83761ccfb3835a9e519d
Marius Darila <marius.darila@gmail.com>
2017-08-30 17:52:32 +0300

    feat(essence): wip

---
+++ contracts/token/Essence.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.0;
+
+
+import './AETH.sol';
+import 'zeppelin-solidity/contracts/ownership/HasNoTokens.sol';
+import 'zeppelin-solidity/contracts/ownership/HasNoEther.sol';
+
+contract Essence is HasNoEther, HasNoTokens {
+    // this is updated frequently by owner
+    bytes32 currentHash;
+
+    mapping(address => mapping(bytes32 => uint256[])) balance;
+
+    function Essence()
+    HasNoEther()
+    HasNoTokens()
+    {
+
+    }
+
+    function newHash(bytes32 _hash)
+    onlyOwner
+    returns (bool)
+    {
+        currentHash = _hash;
+        return bool;
+    }
+
+}
